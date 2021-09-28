8d231651c0b3c0a4ae227cb0b16268917f9b86f9
Chris <cdp12000@gmail.com>
2021-09-27 22:03:28 -0500

    initial commit, sidebar setup

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the front-end react project for Kingsland University.
+
+Run npm install for dependencies 
+ 
+Run npm start to run application in a browser
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './App.css';
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
