9baecb1503fdfec5b84439b9f9950db9fb008628
Chris <cdp12000@gmail.com>
2021-10-06 17:00:52 -0500

    added ability to create a user in the database

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./css/Sidebar.css";
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
 import HomeIcon from '@mui/icons-material/Home';
@@ -11,29 +11,21 @@ import SidebarOption from './SidebarOption';
 // import { Button } from '@mui/material'
 // import { Link, Switch, Route } from 'react-router-dom'
 
-export default function Sidebar ({pageView}) {
-    const initView = {
-        page: "",
-      }
-    
-      const [pageClicked, setPageClicked] = useState(initView);
-    
-      const data = (event) => {
-        event.preventDefault();
-        const { value } = event.target;
-        setPageClicked({...pageClicked, page: value})
-      }
-
-      console.log(pageClicked)
+export default function Sidebar ({thisPage}) {
 
     return (
         
         <div className="sidebar">
             <TwitterIcon className="sidebar__twitterIcon"/>
 
-            <button type="button" page="Home" name="Home" value={pageClicked.page} onClick={data}><SidebarOption active Icon={HomeIcon} text="Home" /></button>
-            <SidebarOption Icon={AppRegistrationIcon} text="Register" />
-            <SidebarOption Icon={LoginIcon} text="Login" />
+            <button className="sidebar__button" type="button" page="Home" name="Home" value="Home" onClick={() => thisPage('Home')}><SidebarOption active Icon={HomeIcon} text="Home" /></button>
+            <button type="button" page="Register" name="Register" value="Register" onClick={() => thisPage('Register')}><SidebarOption active Icon={AppRegistrationIcon}  text="Register" /></button>
+            <button type="button" page="Login" name="Login" value="Login" onClick={() => thisPage('Login')}><SidebarOption active Icon={LoginIcon} text="Login" /></button>
+
+            <SidebarOption active Icon={HomeIcon} text="Home" />
+            <SidebarOption active Icon={AppRegistrationIcon}  text="Register" />
+            <SidebarOption active Icon={LoginIcon} text="Login" />
+            
 
             {/* <Button variant="outlined" className="sidebar_tweet" fullWidth>Tweet</Button> */}
         </div>
