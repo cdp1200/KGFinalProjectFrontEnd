29de08b987e26d8d061d0a27cebc6fa18b0a6dbe
Chris <cdp12000@gmail.com>
2021-10-22 21:28:46 -0500

    deploy

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://enigmatic-stream-10784.herokuapp.com/api",
+  baseURL: "http://enigmatic-stream-10784.herokuapp.com",
   headers: {
     "Content-type": "application/json"
   }
